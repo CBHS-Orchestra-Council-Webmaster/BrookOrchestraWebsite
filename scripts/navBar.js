2c35e942182e15bf633ee75431952e07c24af329
Alan Johnson <alanjcd2004@gmail.com>
2022-03-20 21:55:00 -0500

    added front image and logo

--- scripts/navBar.js
+++ scripts/navBar.js
@@ -4,15 +4,15 @@ function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
-// Close the dropdown menu if the user clicks outside of it
+//closing dropfown if user clicks outside
 window.onclick = function (event) {
     if (!event.target.matches('.dropbtn')) {
         var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
+        for (var i = 0; i < dropdowns.length; i++) {
             var openDropdown = dropdowns[i];
             if (openDropdown.classList.contains('show')) {
                 openDropdown.classList.remove('show');
+
             }
         }
     }
