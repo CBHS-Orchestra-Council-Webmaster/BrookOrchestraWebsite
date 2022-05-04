c328edbcba8137e0b5ebf18e90bd246679952345
Alan Johnson <alanjcd2004@gmail.com>
2022-05-04 00:34:41 -0500

    added to small gallery

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,19 @@
+
+
+
+$(window).scroll(function () {
+    var scrollTop = $(this).scrollTop();
+
+    $('.logo').css({
+        opacity: function () {
+            var elementHeight = $(this).height(),
+                opacity = (((elementHeight - scrollTop) / elementHeight));
+            return opacity;
+        }
+    });
+});
+
+
 const appear = document.querySelector('.appear');
 const cb = function (entries) {
     entries.forEach(entry => {
@@ -10,3 +26,16 @@ const cb = function (entries) {
 }
 const io = new IntersectionObserver(cb);
 io.observe(appear);
+
+const appear2 = document.querySelector('.appear2');
+const cb2 = function (entries) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('inview');
+            entry.unobserve(entry.target);
+
+        }
+    });
+}
+const io2 = new IntersectionObserver(cb2);
+io2.observe(appear2);
